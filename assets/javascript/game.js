0ff971e2d8f17fa5bcf0725b941e0f64b034a011
Maniccia <mariamaniccia@gmail.com>
2018-11-13 13:21:05 -0500

    The start

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+//Global variables
+
+
+$(document).ready(function() {
+    
+    //  Characters
+    let characters = {
+        'JPrince': {
+            name: 'JPrince',
+            health: 180,
+            attack: 10,
+            imageUrl: "",
+            enemyAttackBack: 10
+        }, 
+        'Kanye': {
+            name: 'Kanye',
+            health: 150,
+            attack: 8,
+            imageUrl: "",
+            enemyAttackBack: 10
+        }, 
+        'Drake': {
+            name: 'finn',
+            health: 150,
+            attack: 8,
+            imageUrl: "",
+            enemyAttackBack: 10
+        }, 
+        'PushaT': {
+            name: 'stormtrooper',
+            health: 100,
+            attack: 7,
+            imageUrl: "",
+            enemyAttackBack: 20
+        }
+    };
